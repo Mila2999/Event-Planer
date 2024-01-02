52a714d836ac1fa013d81020461f4889f8889a87
mila2999 <mila.savonik@gmail.com>
2024-01-02 22:59:55 +0200

    Using useLoaderData in Component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,7 +3,7 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const events = useLoaderData();
-  return <>{<EventsList events={events} />}</>;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
