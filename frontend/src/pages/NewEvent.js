852e068d9f74d52eb8f7139471ee15c1e6abd029
mila2999 <mila.savonik@gmail.com>
2024-01-17 13:15:53 +0200

    Handling form submission with redirect()

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,4 +1,4 @@
-import { json } from 'react-router-dom';
+import { json, redirect } from 'react-router-dom';
 import EventForm from '../components/EventForm';
 function NewEventPage() {
   return <EventForm />;
@@ -7,21 +7,19 @@ export default NewEventPage;
 
 export async function action({ request, params }) {
   const data = await request.formData();
-  const eventData = {
-    title: data.get('title'),
-    image: data.get('image'),
-    date: data.get('date'),
-    description: data.get('description'),
-  };
 
   const response = await fetch('http://localhost:8080/events', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(eventData),
+    body: JSON.stringify({
+      title: data.get('title'),
+      image: data.get('image'),
+      date: data.get('date'),
+      description: data.get('description'),
+    }),
   });
   if (!response.ok) {
     throw json({ message: 'Could not save event.' }, { status: 500 });
-  } else {
-    return response;
   }
+  return redirect('/events');
 }
